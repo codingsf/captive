d22fd7a7b049524db453857a4646b1aafe425a8a
Tom Spink <tspink@gmail.com>
2015-02-10 10:36:33 +0000

    Update engine support code

--- engines/test/start.cpp
+++ engines/test/start.cpp
@@ -6,6 +6,6 @@ void printf(const char *fmt, ...);
 
 extern "C" void engine_start(unsigned int entry_point)
 {
-	printf("Welcome to the Engine\n");
+	printf("Welcome to the engine: %s entry point=%x\n", "tom", 0xbabe1234);
 	for(;;);
 }
