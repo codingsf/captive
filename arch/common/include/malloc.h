97b8950dc52cdf185c8c05c6208c4555e7c52c45
Tom Spink <tspink@gmail.com>
2015-03-31 17:18:59 +0100

    Begin porting dlmalloc() into the kernel

--- arch/common/include/malloc.h
+++ arch/common/include/malloc.h
@@ -10,20 +10,24 @@
 
 #include <define.h>
 
+extern "C" void *dlmalloc(size_t);
+extern "C" void *dlrealloc(void *p, size_t);
+extern "C" void *dlcalloc(size_t, size_t);
+extern "C" void dlfree(void *p);
+
 namespace captive {
 	struct MemoryVector;
-	
-	namespace arch {
-		struct malloc_unit
-		{
-			uint32_t size;
-		};
 
+	namespace arch {
 		void malloc_init(MemoryVector& arena);
-		void *malloc(size_t size);
-		void *calloc(size_t nmemb, size_t size);
-		void *realloc(void *p, size_t size);
-		void free(void *p);
+
+		void *malloc(size_t size) { return dlmalloc(size); }
+
+		void *realloc(void *p, size_t size) { return dlrealloc(p, size); }
+
+		void *calloc(size_t nmemb, size_t size) { return dlcalloc(nmemb, size); }
+
+		void free(void *p) { return dlfree(p); }
 	}
 }
 
--- arch/common/malloc/malloc.cpp
+++ arch/common/malloc/malloc.cpp
@@ -5,69 +5,20 @@
 
 using namespace captive::arch;
 
-static uint8_t *heap;
+static uint8_t *heap, *brk;
 static size_t heap_size;
-
-static uint8_t *heap_free_ptr;
-static uint8_t *heap_end_ptr;
 
 void captive::arch::malloc_init(captive::MemoryVector& arena)
 {
-	heap = (uint8_t *)arena.base_address;
+	heap = brk = (uint8_t *)arena.base_address;
 	heap_size = arena.size;
-
-	heap_free_ptr = heap;
-	heap_end_ptr = heap + heap_size;
 }
 
-void *captive::arch::malloc(size_t size)
+extern "C" void *sbrk(int64_t incr)
 {
-	size += sizeof(struct malloc_unit);
-	if (size % 16) {
-		size += 16 - (size % 16);
-	}
+	if (incr == 0)
+		return (void *)brk;
 
-	if (heap_free_ptr > (heap_end_ptr - size)) {
-		return NULL;
-	}
-
-	struct malloc_unit *unit = (struct malloc_unit *)heap_free_ptr;
-	heap_free_ptr += size;
-
-	unit->size = size;
-
-	return (void *)((uint8_t *)unit + sizeof(struct malloc_unit));
-}
-
-void *captive::arch::realloc(void *p, size_t size)
-{
-	if (p == NULL) {
-		return malloc(size);
-	}
-
-	struct malloc_unit *unit = (struct malloc_unit *)((uint8_t *)p - sizeof(struct malloc_unit));
-
-	if (size == unit->size) {
-		return p;
-	}
-
-	void *newmem = malloc(size);
-	if (!newmem) {
-		return NULL;
-	}
-
-	if (size < unit->size) {
-		memcpy(newmem, p, size);
-	} else {
-		memcpy(newmem, p, unit->size);
-	}
-
-	free(p);
-
-	return newmem;
-}
-
-void captive::arch::free(void *p)
-{
-
+	brk += incr;
+	return (void *)brk;
 }
