8285f434ea58895d05ef551bf1b3cb5ad1ba91a3
Harry Wagstaff <fred.the.fish@gmail.com>
2015-07-28 17:05:07 +0100

    Keep a 1-entry region cache when using the block jit

--- arch/common/cpu-block-jit.cpp
+++ arch/common/cpu-block-jit.cpp
@@ -63,6 +63,9 @@ bool CPU::run_block_jit_safepoint()
 {
 	bool step_ok = true;
 	
+	Region *rgn = NULL;
+	uint32_t region_virt_base;
+	
 	do {
 		// Check the ISR to determine if there is an interrupt pending,
 		// and if there is, instruct the interpreter to handle it.
@@ -84,24 +87,29 @@ bool CPU::run_block_jit_safepoint()
 		gva_t virt_pc = (gva_t)read_pc();
 		gpa_t phys_pc;
 
-		// This will perform a FETCH with side effects, so that we can impose the
-		// correct permissions checking for the block we're about to execute.
-		MMU::resolution_fault fault;
-		if (unlikely(!mmu().virt_to_phys(virt_pc, phys_pc, fault))) abort();
+		if(!rgn || PAGE_ADDRESS_OF(virt_pc) != region_virt_base) {
+			// This will perform a FETCH with side effects, so that we can impose the
+			// correct permissions checking for the block we're about to execute.
+			MMU::resolution_fault fault;
+			if (unlikely(!mmu().virt_to_phys(virt_pc, phys_pc, fault))) abort();
 
-		// If there was a fault, then switch back to the safe-point.
-		if (unlikely(fault)) {
-			restore_safepoint(&cpu_safepoint, (int)fault);
+			// If there was a fault, then switch back to the safe-point.
+			if (unlikely(fault)) {
+				restore_safepoint(&cpu_safepoint, (int)fault);
+				
+				// Since we've just destroyed the stack, we should never get here.
+				assert(false);
+			}
 			
-			// Since we've just destroyed the stack, we should never get here.
-			assert(false);
+			// Mark the physical page corresponding to the PC as executed
+			mmu().set_page_executed(VA_OF_GPA(PAGE_ADDRESS_OF(phys_pc)));
+			
+		
+			rgn = image->get_region(phys_pc);
+			region_virt_base = PAGE_ADDRESS_OF(virt_pc);
 		}
 		
-		// Mark the physical page corresponding to the PC as executed
-		mmu().set_page_executed(VA_OF_GPA(PAGE_ADDRESS_OF(phys_pc)));
-		
-		Region *rgn = image->get_region(phys_pc);
-		Block *blk = rgn->get_block(phys_pc);
+		Block *blk = rgn->get_block(PAGE_OFFSET_OF(virt_pc));
 		
 		if (blk->txln) {
 			step_ok = blk->txln(&jit_state) == 0;
@@ -109,7 +117,7 @@ bool CPU::run_block_jit_safepoint()
 		}
 
 		if (blk->exec_count > 10) {
-			blk->txln = compile_block(blk, phys_pc);
+			blk->txln = compile_block(blk, PAGE_ADDRESS_OF(phys_pc) | PAGE_OFFSET_OF(virt_pc));
 			mmu().disable_writes();
 			
 			step_ok = blk->txln(&jit_state) == 0;
