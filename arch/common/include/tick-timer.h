2ece77db58c0ca68a01a28dd7b2af86b95a0d475
Harry Wagstaff <fred.the.fish@gmail.com>
2015-08-03 15:50:21 +0100

    Make tick-timer more useful

--- arch/common/include/tick-timer.h
+++ arch/common/include/tick-timer.h
@@ -3,21 +3,35 @@
 
 #include <vector>
 
+#include <malloc.h>
+#include <string.h>
+
 class tick_timer
 {
 	public:
 		tick_timer(bool enabled = 1) : enabled(enabled) {}
+		~tick_timer() {
+			for(auto i : names) {
+				if(i) captive::arch::free(i);
+			}
+		}
 	
-		void reset() { if(!enabled) return; ticks.clear(); }
-		void tick() { if(!enabled) return; ticks.push_back(__rdtsc()); }
+		void reset() { if(!enabled) return; ticks.clear(); tick("Start"); }
+		void tick(const char *tickname = NULL) { 
+			if(!enabled) return; 
+			ticks.push_back(__rdtsc()); 
+			if(tickname)names.push_back(strdup(tickname));
+			else names.push_back(NULL);
+		}
 		
-		void dump() 
+		void dump(const char *prefix=NULL) 
 		{
 			if(!enabled) return;
+			if(prefix) printf(prefix);
 			uint64_t last = ticks[0];
 			for(uint32_t i = 1; i < ticks.size(); ++i) {
 				uint64_t current = ticks[i];
-				printf("%lu ", current-last);
+				if(names[i])printf("%s: %lu ::", names[i], current-last);
 				last = current;
 			}
 			
@@ -26,6 +40,7 @@ class tick_timer
 		
 	private:
 		std::vector<uint64_t> ticks;
+		std::vector<char *> names;
 		bool enabled;
 };
 
