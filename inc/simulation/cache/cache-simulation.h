adcde3f30ac8270a10041995448ada7479d2d3de
Tom Spink <tspink@gmail.com>
2016-09-09 15:21:24 +0100

    Fix the CPU cache representation

--- inc/simulation/cache/cache-simulation.h
+++ inc/simulation/cache/cache-simulation.h
@@ -23,131 +23,131 @@ namespace captive
 	{
 		namespace cache
 		{
-			template<uint32_t total_size, uint32_t line_size, uint8_t ways, bool vidx, bool vtag, bool nonseq>
+			template<uint32_t total_size, uint32_t line_size, uint8_t number_of_ways, bool vidx, bool vtag, bool nonseq>
 			struct CPUCache
 			{
 				public:
 					CPUCache() : read_hits(0), read_misses(0), write_hits(0), write_misses(0), rrp(0x9E3779B9), last_line(-1) { }
+				
+				private:
+					static constexpr int number_of_lines = (int)(total_size / line_size);
+					static constexpr int log2_number_of_lines = (int)std::log2(number_of_lines);
 					
-				struct number_of_lines
-				{
-					enum { value = (int)(total_size / line_size) };
-					enum { lg2 = (int)std::log2(total_size / line_size) };
-				};
-				
-				struct offset_bits
-				{
-					enum { value = (int)std::log2(line_size), mask = ((1 << (int)std::log2(line_size)) - 1) };
-				};
-				
-				struct index_bits
-				{
-					enum { value = number_of_lines::lg2, mask = ((1 << number_of_lines::lg2) - 1) };
-				};
-				
-				struct tag_bits
-				{
-					enum
+					static constexpr int number_of_sets = (int)(number_of_lines / number_of_ways);
+					static constexpr int log2_number_of_sets = (int)std::log2(number_of_sets);
+					
+					static constexpr int log2_number_of_ways = (int)std::log2(number_of_ways);
+					
+					static constexpr int offset_bits = (int)std::log2(line_size);
+					static constexpr int offset_mask = ((1 << offset_bits) - 1);
+					
+					static constexpr int set_bits = log2_number_of_sets;
+					static constexpr int set_mask = ((1 << set_bits) - 1);
+					
+					static constexpr int tag_bits = 31 - (set_bits + offset_bits);
+					static constexpr int tag_mask = ((1 << tag_bits) - 1);
+
+					uint32_t tags[number_of_lines];
+					uint32_t rrp;
+					uint32_t last_line;
+
+					inline constexpr uint32_t cache_line(uint32_t vaddr, uint32_t paddr) const __attribute__((pure))
 					{
-						value = 31 - index_bits::value - offset_bits::value,
-						mask = ((1 << value) - 1),
-					};
-				};
-								
-				uint64_t read_hits, read_misses, write_hits, write_misses;
-				uint32_t tags[number_of_lines::value * ways];
-				uint32_t rrp;
-				uint32_t last_line;
-				
-				inline uint32_t cache_line(uint32_t vaddr, uint32_t paddr) const __attribute__((pure))
-				{
-					return index_of(vaddr, paddr);
-				}
-				
-				inline constexpr uint32_t tag_addr(uint32_t vaddr, uint32_t paddr) const __attribute__((pure))
-				{
-					return vtag ? vaddr : paddr;
-				}
-						
-				inline constexpr uint32_t index_addr(uint32_t vaddr, uint32_t paddr) const __attribute__((pure))
-				{
-					return vidx ? vaddr : paddr;
-				}
-						
-				inline constexpr uint32_t tag_of(uint32_t vaddr, uint32_t paddr) const __attribute__((pure))
-				{
-					return (tag_addr(vaddr, paddr) >> (index_bits::value + offset_bits::value)) & tag_bits::mask;
-				}
-				
-				inline constexpr uint32_t index_of(uint32_t vaddr, uint32_t paddr) const __attribute__((pure))
-				{
-					return (index_addr(vaddr, paddr) >> offset_bits::value) & index_bits::mask;
-				}
-				
-				inline uint32_t tag_at(uint32_t vaddr, uint32_t paddr, uint8_t way) const
-				{				
-					return tags[index_of(vaddr, paddr) + number_of_lines::value * way];
-				}
-				
-				inline void tag_set(uint32_t vaddr, uint32_t paddr, uint8_t way)
-				{				
-					tags[index_of(vaddr, paddr) + number_of_lines::value * way] = tag_of(vaddr, paddr);
-				}
-				
-				inline bool hit(uint32_t vaddr, uint32_t paddr) const
-				{
-					if (ways == 2) {
-						return tag_at(vaddr, paddr, 0) == tag_of(vaddr, paddr) || 
-								tag_at(vaddr, paddr, 1) == tag_of(vaddr, paddr);
-					} else if (ways == 4) {
-						return tag_at(vaddr, paddr, 0) == tag_of(vaddr, paddr) || 
-								tag_at(vaddr, paddr, 1) == tag_of(vaddr, paddr) ||
-								tag_at(vaddr, paddr, 2) == tag_of(vaddr, paddr) ||
-								tag_at(vaddr, paddr, 3) == tag_of(vaddr, paddr);
+						return set_of(vaddr, paddr);
 					}
-				}
-				
-				inline void replace(uint32_t vaddr, uint32_t paddr)
-				{
-					if (ways == 2) tag_set(vaddr, paddr, rrp & 1);
-					else if (ways == 4) tag_set(vaddr, paddr, rrp & 2);
+
+					inline constexpr uint32_t tag_addr(uint32_t vaddr, uint32_t paddr) const __attribute__((pure))
+					{
+						return vtag ? vaddr : paddr;
+					}
+
+					inline constexpr uint32_t set_addr(uint32_t vaddr, uint32_t paddr) const __attribute__((pure))
+					{
+						return vidx ? vaddr : paddr;
+					}
+
+					inline constexpr uint32_t tag_of(uint32_t vaddr, uint32_t paddr) const __attribute__((pure))
+					{
+						return (tag_addr(vaddr, paddr) >> (set_bits + offset_bits)) & tag_mask;
+					}
+
+					inline constexpr uint32_t set_of(uint32_t vaddr, uint32_t paddr) const __attribute__((pure))
+					{
+						return (set_addr(vaddr, paddr) >> offset_bits) & set_mask;
+					}
 					
-					rrp = ((rrp & 1) << 31) | (rrp >> 1);
-				}
-				
-				inline bool read(uint32_t vaddr, uint32_t paddr, uint8_t sz)
-				{
-					if (hit(vaddr, paddr)) {
-						if (nonseq) {
-							if (cache_line(vaddr, paddr) == last_line) return true;
-							last_line = cache_line(vaddr, paddr);
-						}
-						
-						read_hits++;
-						return true;
-					} else {
-						read_misses++;
-						replace(vaddr, paddr);
-						return false;
+					inline constexpr int tag_slot(uint32_t vaddr, uint32_t paddr, uint8_t way) const __attribute__((pure))
+					{
+						//return tags[index_of(vaddr, paddr) + number_of_lines::value * way];
+						return (set_of(vaddr, paddr) << log2_number_of_ways) + way;
 					}
-				}
-				
-				inline bool write(uint32_t vaddr, uint32_t paddr, uint8_t sz)
-				{
-					if (hit(vaddr, paddr)) {
-						if (nonseq) {
-							if (cache_line(vaddr, paddr) == last_line) return true;
-							last_line = cache_line(vaddr, paddr);
-						}
-						
-						write_hits++;
-						return true;
-					} else {
-						write_misses++;
-						replace(vaddr, paddr);
-						return false;
+
+					inline bool tag_match(uint32_t vaddr, uint32_t paddr, uint8_t way) const
+					{				
+						return tags[tag_slot(vaddr, paddr, way)] == tag_of(vaddr, paddr);
 					}
-				}
+
+					inline void update_tag(uint32_t vaddr, uint32_t paddr, uint8_t way)
+					{				
+						tags[tag_slot(vaddr, paddr, way)] = tag_of(vaddr, paddr);
+					}
+
+					inline bool hit(uint32_t vaddr, uint32_t paddr) const
+					{
+						if (number_of_ways == 2) {
+							return tag_match(vaddr, paddr, 0) || tag_match(vaddr, paddr, 1);
+						} else if (number_of_ways == 4) {
+							return tag_match(vaddr, paddr, 0) || 
+									tag_match(vaddr, paddr, 1) ||
+									tag_match(vaddr, paddr, 2) ||
+									tag_match(vaddr, paddr, 3);
+						}
+					}
+
+					inline void replace(uint32_t vaddr, uint32_t paddr)
+					{
+						if (number_of_ways == 2) update_tag(vaddr, paddr, rrp & 1);
+						else if (number_of_ways == 4) update_tag(vaddr, paddr, rrp & 2);
+
+						rrp = ((rrp & 1) << 31) | (rrp >> 1);
+					}
+				
+				public:
+					uint64_t read_hits, read_misses, write_hits, write_misses;
+
+					inline bool read(uint32_t vaddr, uint32_t paddr, uint8_t sz)
+					{
+						if (hit(vaddr, paddr)) {
+							if (nonseq) {
+								if (cache_line(vaddr, paddr) == last_line) return true;
+								last_line = cache_line(vaddr, paddr);
+							}
+
+							read_hits++;
+							return true;
+						} else {
+							read_misses++;
+							replace(vaddr, paddr);
+							return false;
+						}
+					}
+
+					inline bool write(uint32_t vaddr, uint32_t paddr, uint8_t sz)
+					{
+						if (hit(vaddr, paddr)) {
+							if (nonseq) {
+								if (cache_line(vaddr, paddr) == last_line) return true;
+								last_line = cache_line(vaddr, paddr);
+							}
+
+							write_hits++;
+							return true;
+						} else {
+							write_misses++;
+							replace(vaddr, paddr);
+							return false;
+						}
+					}
 			};
 			
 			class CacheSimulation : public Simulation
